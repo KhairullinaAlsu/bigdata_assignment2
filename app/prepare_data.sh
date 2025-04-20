706c42d74342970fbb5fa373c5be50c163e01b2b
KhairullinaAlsu <97334258+KhairullinaAlsu@users.noreply.github.com>
2025-04-20 22:13:17 +0300

    Update prepare_data.sh

--- app/prepare_data.sh
+++ app/prepare_data.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 source .venv/bin/activate
-
-
-# Python of the driver (/app/.venv/bin/python)
 export PYSPARK_DRIVER_PYTHON=$(which python) 
-
-
 unset PYSPARK_PYTHON
-
-# DOWNLOAD a.parquet or any parquet file before you run this
-
-hdfs dfs -put -f a.parquet / && \
+# DOWNLOAD b.parquet or any parquet file before you run this
+hdfs dfs -put -f b.parquet / && \
     spark-submit prepare_data.py && \
     echo "Putting data to hdfs" && \
     hdfs dfs -put data / && \
